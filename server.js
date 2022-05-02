309afb1968482f824b553a83863ffe5834325eaf
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-05-02 19:32:09 +0100

    add first GET route

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 
 // can now access methods used for making a server by including their name after app
 
+const mockUserData=[
+  {name: 'Mark'},
+  {name: 'Jill'}
+]
+app.get('/users', function(req,res){
+  res.json({
+    success: true,
+    message: 'successfully got users. Nice!',
+    users: mockUserData
+  })
+})
+
 // starts up the server locally on port 8000
 // run this with terminal commande 'node server.js'
 // Ctrl+C to end
