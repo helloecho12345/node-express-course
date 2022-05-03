6c2b15ca9cc6bbcab54535d051721b04a3cf9ddf
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-05-03 23:40:59 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 // can now access methods used for making a server by including their name after app
 
+// first endpoint
+// this function will respond to a GET request at http://localhost:8000/users with a JSON file, which includes mockData under the key 'users'
 const mockUserData=[
   {name: 'Mark'},
   {name: 'Jill'}
@@ -16,6 +18,19 @@ app.get('/users', function(req,res){
     users: mockUserData
   })
 })
+
+// In Express, words with a colon in front of them in the url are treated as variables. You can access the value of each variable through req.params
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+// OUTPUT : {"success":true,"message":"got one user","user":"mark"}
+// check the terminal for the console.log of req.params.id. You should see the name 'mark', since it is the value we passed in place of the 'id' variable. Since we are running this in the terminal, our console.log will appear there (instead of the browser).
 
 // starts up the server locally on port 8000
 // run this with terminal commande 'node server.js'
